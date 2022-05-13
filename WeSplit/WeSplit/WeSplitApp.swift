79f87676f25e9bc03cb2cc6aee8a143b42ce7430
Jordan Kramer <45769925+blinKAlliance@users.noreply.github.com>
2022-05-13 15:50:38 -0500

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jordan Kramer on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
